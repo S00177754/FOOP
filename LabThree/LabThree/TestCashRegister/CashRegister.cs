b55cfadb6c2fd9ebf468b52d78318998ff5ad020
Nanopulse1 <36006416+Nanopulse1@users.noreply.github.com>
2018-09-30 12:41:21 +0100

    Lab 3 Initial Commit
    -Completed question one
    -Added word document with exercises included
    -Question Twocurrently in progress

---
+++ LabThree/LabThree/TestCashRegister/CashRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCashRegister
+{
+    class CashRegister
+    {
+        public decimal total;
+        public int quantity;
+
+        public void AddItem(double price)
+        {
+
+        }
+    }
+}

ff43ae33ef3762247dba002a74bc637d3bd4da32
Nanopulse1 <36006416+Nanopulse1@users.noreply.github.com>
2018-10-02 10:54:52 +0100

    Lab 3 02/10/18
    -Completed Lab Three Q3
    - Q2 & Q1 need to be revised and optimised now that I have a better understanding of how classes and readonly works

--- LabThree/LabThree/DemoSquares/Program.cs
+++ LabThree/LabThree/DemoSquares/Program.cs
@@ -11,6 +11,13 @@ namespace DemoSquares
         static void Main(string[] args)
         {
             int[] sqObj = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 1; i <= sqObj.Length; i++)
+            {
+                Square SQ = new Square(i); 
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- LabThree/LabThree/DemoSquares/Square.cs
+++ LabThree/LabThree/DemoSquares/Square.cs
@@ -8,9 +8,26 @@ namespace DemoSquares
 {
     class Square
     {
-        int area = 0;
-        int length = 0;
+        private readonly int area = 0;
+        private readonly int length = 0;
 
-        
+        public Square(int sqLength)
+        {
+            length = sqLength;
+            area = CalculateArea(length);
+            Display();
+
+        }
+
+        private int CalculateArea(int squareLength)
+        {
+            squareLength *= squareLength;
+            return squareLength;
+        }
+
+        private void Display()
+        {
+            Console.WriteLine($"The area of a {length} by {length} square is {area}");
+        }
     }
 }

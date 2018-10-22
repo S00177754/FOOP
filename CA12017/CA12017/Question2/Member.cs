68ced531d5c9c725e6a17b7d206c197fd3752699
Nanopulse1 <36006416+Nanopulse1@users.noreply.github.com>
2018-10-22 12:57:06 +0100

    CA 2017 Practice
    -Initial Commit of CA2017
    - Question One Completed
    - Question Two Started

---
+++ CA12017/CA12017/Question2/Member.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Member
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string MemberID { get; set; }
+        public bool MembershipStatus = true;
+
+        public Member(string firstName, string lastName, int age, string ID)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            MemberID = ID;
+            MembershipStatus = false;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"First Name: { FirstName}");
+            Console.WriteLine($"Last Name: { LastName}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"ID: {MemberID}");
+            Console.WriteLine($"Membership Status: {MembershipStatus}");
+            return base.ToString();
+        }
+    }
+}
---
+++ CA12017/CA12017/Question2/MemberTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class MemberTests
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            Member mem = new Member("Ronan", "Cassidy", 19,"S00177754");
+            Member[] members = new Member[5];
+
+            members[0] = new Member("Ronan", "Cassidy", 19, "S00177754");
+            members[1] = new Member("Ronan", "Cassidy", 20, "S00177753");
+            members[2] = new Member("Ronan", "Cassidy", 21, "S00177752");
+            members[3] = new Member("Ronan", "Cassidy", 22, "S00177751");
+            members[4] = new Member("Ronan", "Cassidy", 23, "S00177750");
+
+            mem.ToString();
+
+            Console.ReadKey();
+        }
+    }
+}

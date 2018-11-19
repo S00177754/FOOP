0729bf87b5d2eff37aab968497119cde8e840b43
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2018-11-19 12:53:35 +0000

    Initial Commit - Question One Completed - Question Two In Progress

--- LabEight/LabEight/MainWindow.xaml.cs
+++ LabEight/LabEight/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace LabEight
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Variables
+        Random rng = new Random();
+        int initialRandomNum = 0;
+        int secondRandomNum = 0;
+        string userChoice = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            initialRandomNum = rng.Next(1, 20);
+            txboxInitialNum.Text = $"Initial Number: {initialRandomNum}";
+        }
+
+        private void RdbtnBigger_Checked(object sender, RoutedEventArgs e)
+        {
+            userChoice = "Bigger";
+        }
+
+        private void RdbtnSmaller_Checked(object sender, RoutedEventArgs e)
+        {
+            userChoice = "Smaller";
+        }
+
+        private void BtnGuess_Click(object sender, RoutedEventArgs e)
+        {
+            secondRandomNum = rng.Next(1,20);
+            txtboxSecondNum.Text = $"Second Number: {secondRandomNum}";
+
+            #region Win Logic
+            if (userChoice == "Bigger" && secondRandomNum > initialRandomNum)
+            {
+                txtboxOutcome.Text = "You Win!";
+            }
+            else if (userChoice == "Smaller" && secondRandomNum < initialRandomNum)
+            {
+                txtboxOutcome.Text = "You Win!";
+            }
+            else if (userChoice == "Bigger" && secondRandomNum < initialRandomNum)
+            {
+                txtboxOutcome.Text = "You Lose!";
+            }
+            else if (userChoice == "Smaller" && secondRandomNum > initialRandomNum)
+            {
+                txtboxOutcome.Text = "You Lose!";
+            }
+            #endregion
         }
     }
 }

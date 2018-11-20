73c4658af6d22a6c4cb6ad1b6909dda3948e1732
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2018-11-20 04:11:06 +0000

    Lab Eight Update -ExerciseSeven Renamed To DnDApp -Further Functionality To Be Added  >Export and Import Options  >Further data fields

--- LabEight/ExerciseSeven/MainWindow.xaml.cs
+++ LabEight/ExerciseSeven/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ExerciseSeven
 {
@@ -20,26 +21,63 @@ namespace ExerciseSeven
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Variables
+        List<Player> players = new List<Player>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //Creates New Player based on string in input field
         private void BtnPlayerAdd_Click(object sender, RoutedEventArgs e)
         {
             if (txtboxAddPlayer.Text != null && txtboxAddPlayer.Text != "")
             {
-                Player ply = new Player(txtboxAddPlayer.Text);
-                listbxPLayerList.Items.Add(ply.Name);
+                listbxPLayerList.Items.Add(txtboxAddPlayer.Text);
+                players.Add(new Player(txtboxAddPlayer.Text));
             }
         }
 
+        //Deletes and removes player from list 
         private void BtnDeletePlayer_Click(object sender, RoutedEventArgs e)
         {
             if(listbxPLayerList.SelectedItem != null)
             {
-                listbxPLayerList.Items.RemoveAt(listbxPLayerList.Items.IndexOf(listbxPLayerList.SelectedItem));
-                listbxPLayerList.SelectedItem = null;
+                int selectedIndex = listbxPLayerList.SelectedIndex;
+                listbxPLayerList.Items.RemoveAt(selectedIndex);
+                players.RemoveAt(selectedIndex);
+            }
+        }
+
+        //Loads player data from list and populates fields
+        private void BtnViewPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (listbxPLayerList.SelectedItem != null)
+            {
+                int selectedIndex = listbxPLayerList.SelectedIndex;
+
+                //Data fields filled by following chunk of code.
+                lblPlayerName.Content = $"Name: {players[selectedIndex].Name}";
+                txtblkClass.Text = (players[selectedIndex].Class).ToString();
+                txtblkRace.Text = (players[selectedIndex].Race).ToString();
+                txtblkAC.Text = (players[selectedIndex].AC).ToString();
+                txtblkMaxHealth.Text = (players[selectedIndex].MaxHealth).ToString();
+                txtblkHealth.Text = (players[selectedIndex].Health).ToString();
+            }
+            else
+            {
+                lblPlayerName.Content = "Name:";
+            }
+        }
+
+        //updates fields so that values are not reset on view beingclicked
+        private void BtnSavePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (listbxPLayerList.SelectedItem != null)
+            {
+                int selectedIndex = listbxPLayerList.SelectedIndex;
+                players[selectedIndex].UpdatePlayer(txtblkClass.Text, txtblkRace.Text, int.Parse(txtblkAC.Text), int.Parse(txtblkMaxHealth.Text), int.Parse(txtblkHealth.Text));
             }
         }
     }
@@ -63,6 +101,25 @@ namespace ExerciseSeven
             AC = 12;
             MaxHealth = 6;
             Health = 6;
+        }
+
+        //Methods
+        public void UpdatePlayer(string plyClass,string race, int ac,int maxHealth, int health)
+        {
+            Class = plyClass;
+            Race = race;
+            AC = ac;
+            MaxHealth = maxHealth;
+            Health = health;
+
+        }
+
+        /// <summary>
+        /// Creates string in csv format for exporting of data via text file
+        /// </summary>
+        public string FormatExport(Player pl)
+        {
+            return string.Format($"{Name},{Race},{AC},{MaxHealth},{Health}");
         }
     }
 }

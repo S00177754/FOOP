d419f3b575da86521b15ecd9b4f463fef5f45991
Ronan Cassidy <S00177754@mail.itsligo.ie>
2019-02-09 20:03:57 +0000

    Worked On Cooking Project

---
+++ SEMTwo/Project/FoopProjectS001777754/FoopProjectS001777754/Recipe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoopProjectS001777754
+{
+    public class Recipe
+    {
+        //Variables
+        public string Name { get; private set; }
+        public TimeSpan CookingTime { get; private set; }
+        public TimeSpan PrepTime { get; private set; }
+        public TimeSpan TotalTime { get { return CookingTime + PrepTime; } }
+        public string MethodSteps { get; set; }
+        
+
+        //Constructor
+        public Recipe(string name,TimeSpan cookingTime, TimeSpan prepTime,string method)
+        {
+            Name = name;
+            CookingTime = cookingTime;
+            PrepTime = prepTime;
+            MethodSteps = method;
+        }
+    }
+
+    public class Ingredient
+    {
+        //Enum
+        public enum Measurement { Cups,Kilograms,Grams,Litres, Millilitres, Teaspoons,Tablespoons,Gallon,Quarts,Ounces,Pint,Pound,FluidOunces,Units}
+
+        //Variable
+        public double Amount { get; set; }
+        public Measurement Type { get; set; }
+        public string ingredientName { get; set; }
+
+        //Constructor
+        public Ingredient(double amount,Measurement type,string IngredientName)
+        {
+
+        }
+    }
+}
